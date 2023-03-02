ce366f5c3ee743810cf3a0f0477fe3c97d1a8d42
lararjason <jason.norda@lbs.se>
2023-03-02 13:42:24 +0100

    js uppdatering

--- src/functionplot.js
+++ src/functionplot.js
@@ -2,30 +2,37 @@ console.log('!');
 
 import functionPlot from 'function-plot'
 
+function plot(){
+  var userFn = document.getElementById("fn").value;
+
+  const options = {
+    target: '#root',
+    xAxis: { domain: [0, 3] },
+    yAxis: { domain: [0, 5] },
+    annotations: [
+      { x: 1, text: 'a' },
+      { x: 2, text: 'b' }
+    ],
+    data: [
+      {
+        fn: userFn
+      },
+      {
+        fn: userFn,
+        range: [1, 2],
+        nSamples: 30,
+        closed: true
+      }
+    ]
+  }
+
+  functionPlot(options)
+}
+
 window.onload = (event) => {
     console.log("Hello World!");
-    const options = {
-      target: '#root',
-      xAxis: { domain: [0, 3] },
-      yAxis: { domain: [0, 5] },
-      annotations: [
-        { x: 1, text: 'a' },
-        { x: 2, text: 'b' }
-      ],
-      data: [
-        {
-          fn: 'x * x'
-        },
-        {
-          fn: 'x * x',
-          range: [1, 2],
-          nSamples: 30,
-          closed: true
-        }
-      ]
-    }
-  
-  functionPlot(options)
+    var btn = document.getElementById("btn");
+    btn.addEventListener("click", plot);
 };
 
 
